ad81d37c00f2f936f37e009c3ebecf4d32883c46
Angel Pedroza <angel.pedroza@rappi.com>
2020-10-19 15:35:27 -0500

    [Add] All mandatory tasks

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Typing project."""
+
+
+def sum_list(input_list: list) -> float:
+    """Sum the values of a list"""
+    return sum(input_list)
---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Typing project."""
+
+
+def sum_mixed_list(mxd_lst: list) -> float:
+    """Sum the values of a list with different types of elements"""
+    return sum(mxd_lst)
