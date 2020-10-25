3a97f8e2b178598a2e66d777d15fd865a76053e0
Angel Pedroza <angel.pedroza@rappi.com>
2020-10-25 16:10:39 -0500

    [Fix] Change put return

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -13,27 +13,26 @@ class FIFOCache(BaseCaching):
 
     def put(self, key, item):
         """Assing a key to a value"""
-        if key is not None or item is not None:
+        if key is None or item is None:
+            return
 
-            if key not in self.stack:
-                self.stack.append(key)
-            else:
-                self.move_to_last_in(key)
+        if key not in self.stack:
+            self.stack.append(key)
+        else:
+            self.move_to_last_in(key)
 
-            self.cache_data[key] = item
+        self.cache_data[key] = item
 
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                to_discard = self.stack[0]
-                if to_discard:
-                    self.stack.remove(to_discard)
-                    del self.cache_data[to_discard]
-                    print("DISCARD: {}".format(to_discard))
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            to_discard = self.stack[0]
+            if to_discard:
+                self.stack.remove(to_discard)
+                del self.cache_data[to_discard]
+                print("DISCARD: {}".format(to_discard))
 
     def get(self, key):
         """ return the value in self.cache_data linked to key."""
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        return self.cache_data.get(key, None)
 
     def move_to_last_in(self, key):
         """Move an element to the init the list"""
