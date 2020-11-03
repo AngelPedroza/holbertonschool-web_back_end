f835cb5452f9aded0f7d41bd9bc35a060504fcca
Angel Pedroza <angel.pedroza@rappi.com>
2020-11-02 22:07:39 -0500

    [add] task 6, validate

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -8,3 +8,10 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode(), salt)
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Verify if the hash is correct"""
+    if bcrypt.checkpw(password.encode(), hashed_password):
+        return True
+    return False
