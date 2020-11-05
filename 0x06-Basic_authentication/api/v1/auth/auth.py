45b9450b7ba698ee1b0a267132d4314bcc7c18a3
Angel Pedroza <angel.pedroza@rappi.com>
2020-11-05 18:50:16 -0500

    [add] Tests task 4

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -12,6 +12,15 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Returns False
         """
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        slash = True if path[-1] == '/' else False
+        path = path if slash is True else path + '/'
+
+        if path not in excluded_paths:
+            return True
+
         return False
 
     def authorization_header(self, request=None) -> str:
