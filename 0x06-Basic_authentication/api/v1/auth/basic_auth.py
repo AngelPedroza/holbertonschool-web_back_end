def6408659d42121eb86d6a34aca6c80b7874fa4
Angel Pedroza <angel.pedroza@rappi.com>
2020-11-05 20:27:23 -0500

    [add] Test task 7

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,3 +9,12 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ Basic Auth class
     """
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """ Extract the base64 auth
+        """
+        if authorization_header is not None and \
+                str(authorization_header).split(" ")[0] == 'Basic':
+            return authorization_header.split(" ")[1]
+
+        return None
