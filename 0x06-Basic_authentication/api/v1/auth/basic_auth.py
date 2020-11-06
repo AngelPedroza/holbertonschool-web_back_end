6085806a1839b314590e52e074e908ba9a2d1f92
Angel Pedroza <angel.pedroza@rappi.com>
2020-11-05 20:59:01 -0500

    [add] Tests task 9

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -35,3 +35,17 @@ class BasicAuth(Auth):
             return decode
         except (AttributeError, ValueError) as a:
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+        """ Extract the user credentials
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        return tuple(decoded_base64_authorization_header.split(":"))
