495492a707782f557620232d3ef2bed9214665b8
Angel Pedroza <angel.pedroza@rappi.com>
2020-11-05 19:57:33 -0500

    [add] Test task 5 and fix pycodestyle

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -44,8 +44,11 @@ def forbidden(error) -> str:
 def before_request() -> str:
     """ All before execute the main request
     """
-    exclude_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
-    if auth is not None and auth.require_auth(request.path, exclude_paths) is True:
+    exclude_paths = ['/api/v1/status/',
+                     '/api/v1/unauthorized/',
+                     '/api/v1/forbidden/']
+    if auth is not None and \
+            auth.require_auth(request.path, exclude_paths) is True:
 
         if auth.authorization_header(request) is None:
             abort(401)
