976890540a3111c6c25e271a23b6d074daa6ec3e
Angel Pedroza <angel.pedroza@rappi.com>
2020-11-16 16:21:18 -0500

    [Add] db class

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -23,11 +23,11 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a new user
-        :returns: The new user like a object
+        """ Adds user to database
+        Return: User Object
         """
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
         self._session.commit()
 
-        return new_user
+        return user
