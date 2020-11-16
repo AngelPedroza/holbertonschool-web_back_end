c836613ee93538422f3ad6db75a875356c1cd017
Angel Pedroza <angel.pedroza@rappi.com>
2020-11-16 16:58:35 -0500

    [Add] auth function

---
+++ 0x08-user_authentication_service/auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Auth class"""
+import bcrypt
+
+
+def _hash_password(password: str) -> str:
+    """Convert to a hash
+    """
+    return bcrypt.hashpw(password=password.encode())
