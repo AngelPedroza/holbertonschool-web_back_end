b923680d282c561745395667c67103bd56fbf14c
Angel Pedroza <angel.pedroza@rappi.com>
2020-11-16 16:22:54 -0500

    [Add] db class

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -9,6 +9,7 @@ from user import Base, User
 
 class DB:
     """DB class"""
+
     def __init__(self):
         self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
@@ -17,17 +18,18 @@ class DB:
 
     @property
     def _session(self):
+        """Session getter """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ Adds user to database
-        Return: User Object
+        """Add a new user
+        :returns: The new user like a object
         """
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
         self._session.commit()
 
-        return user
+        return new_user
