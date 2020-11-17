fad75803d20e0325b0f6a70e96e0f657700d620a
Angel Pedroza <angel.pedroza@rappi.com>
2020-11-16 19:22:08 -0500

    [Add] Register an user by an endpoint

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,14 +1,33 @@
 #!/usr/bin/env python3
 """Flask App"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=['GET'], strict_slashes=False)
 def main():
     """Main execute"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def get_user():
+    """Get an user"""
+    email = request.form.get('email')
+    pwd = request.form.get('password')
+
+    if email is None or pwd is None:
+        abort(400)
+
+    try:
+        user = AUTH.register_user(email=email, password=pwd)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    else:
+        return jsonify({"email": f"{user.email}", "message": "user created"}), 200
 
 
 if __name__ == "__main__":
