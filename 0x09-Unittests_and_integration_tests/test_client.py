11180da2c154e4280285157fbe04f9eab7e5f0ca
Angel Pedroza <angel.pedroza@rappi.com>
2020-11-22 20:19:19 -0500

    [add] Call the public repos, pass value through the function and verify it

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -38,3 +38,25 @@ class TestGithubOrgClient(unittest.TestCase):
             test_class = GithubOrgClient('test')
             res = test_class._public_repos_url
             self.assertEqual(res, mock.return_value['repos_url'])
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_json):
+        """
+        Test public repos
+        :return: Nothing
+        """
+        test_payload = [{"name": "test_1"},  {"name": "test_2"}]
+        mock_json.return_value = test_payload
+
+        with patch.object(GithubOrgClient, '_public_repos_url',
+                          new_callable=PropertyMock) as mock:
+            mock.return_value = "test/value"
+            test_class = GithubOrgClient('test')
+            list_test = test_class.public_repos()
+
+            verify_dict = [
+                {"name": i} for i in list_test
+            ]
+            self.assertEqual(verify_dict, test_payload)
+            mock.assert_called_once()
+            mock_json.assert_called_once()
