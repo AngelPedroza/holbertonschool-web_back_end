371d59437db6b0baf4332061062a57096d4629f3
Angel Pedroza <angel.pedroza@rappi.com>
2020-11-30 11:07:04 -0500

    [add] f*ck this sh*t

--- 0x0A-i18n/6-app.py
+++ 0x0A-i18n/6-app.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
-"""My flask app
-"""
-from flask import (Flask, render_template,
-                   request, g)
-from flask_babel import Babel
+""" Module for trying out Babel i18n """
+from flask_babel import Babel, _
+from flask import Flask, render_template, request, g
+from typing import Union
+
+app = Flask(__name__, template_folder='templates')
+babel = Babel(app)
 
 
 class Config(object):
-    """The config class"""
+    """ Configuration Class for Babel """
 
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-app = Flask(__name__)
 app.config.from_object(Config)
-babel = Babel(app)
 
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
@@ -26,9 +26,12 @@ users = {
 }
 
 
-def get_user() -> dict or None:
-    """Get the user
+def get_user() -> Union[dict, None]:
+    """ Returns a user dictionary or
+    None if the ID cannot be found or
+    if login_as was not passed.
     """
+
     try:
         login_as = request.args.get("login_as")
         user = users[int(login_as)]
@@ -38,33 +41,36 @@ def get_user() -> dict or None:
     return user
 
 
+@app.before_request
+def before_request():
+    """ Operations that happen before any request """
+    user = get_user()
+    g.user = user
+
+
 @app.route('/', methods=['GET'], strict_slashes=False)
-def main():
-    """Main function to test i18n
-    """
-    return render_template('6-index.html', user=g.get('user'))
+def hello_world() -> str:
+    """Renders a Basic Template for Babel Implementation"""
+    return render_template("6-index.html")
 
 
 @babel.localeselector
-def get_locale():
-    """Get locale
-    """
-    user: dict = get_user()
-    if user:
-        locale: str or None = user.get('locale')
-        if locale is not None and locale in app.config['LANGUAGES']:
+def get_locale() -> str:
+    """Select a language translation to use for that request"""
+    locale = request.args.get("locale")
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+
+    if g.user:
+        locale = g.user.get("locale")
+        if locale and locale in app.config['LANGUAGES']:
             return locale
 
+    locale = request.headers.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-@app.before_request
-def before_request():
-    """Execution before to call the endpoint
-    """
-    user: dict = get_user()
-    if user is not None:
-        g.user: dict = user
 
 
 if __name__ == "__main__":
