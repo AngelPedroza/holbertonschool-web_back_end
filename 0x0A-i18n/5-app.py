fde53d2a5bd32d5315f1d7cae06ba23b7f13155a
Angel Pedroza <angel.pedroza@rappi.com>
2020-11-30 11:01:57 -0500

    [add] idk

--- 0x0A-i18n/5-app.py
+++ 0x0A-i18n/5-app.py
@@ -29,7 +29,9 @@ users = {
 
 
 def get_user() -> Union[dict, None]:
-    """Get the user that is called from the URI with the endpoint
+    """ Returns a user dictionary or
+        None if the ID cannot be found or
+        if login_as was not passed.
     """
     try:
         login_as = request.args.get("login_as")
