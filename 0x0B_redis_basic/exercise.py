51efef020677ec64f770d1cbad0c81b3487f1ea9
Angel Pedroza <angel.pedroza@rappi.com>
2020-12-01 12:12:45 -0500

    Module comment more large

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-""" My cache class module """
+""" Module to study Redis and practice """
 import redis
 from uuid import uuid4
 from typing import Union
 
 
 class Cache:
-    """ My cache class for redis """
+    """ Class to implement cache strategy with redis """
 
     def __init__(self):
         """ Constructor method """
@@ -14,8 +14,7 @@ class Cache:
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """
-        Get a data that will be saved in redis like value of
+        """Get a data that will be saved in redis like value of
         a random key that will be created with uuid.
         """
         key = str(uuid4())
