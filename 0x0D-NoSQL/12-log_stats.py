79eee3629d44c05c822bb3f864df345998413fcd
Angel Pedroza <angel.pedroza@rappi.com>
2020-12-08 22:22:07 -0500

    [add] task 12

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -2,15 +2,17 @@
 """Print info in a collection"""
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-collention = client.logs.nginx
+if __name__ == "__main__":
+    """ Make a check for all elements in a collention """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collention = client.logs.nginx
 
-print(f"{collention.estimated_document_count()} logs")
+    print(f"{collention.estimated_document_count()} logs")
 
-print("Methods:")
-for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-    method_count = collention.count_documents({'method': method})
-    print(f"\tmethod {method}: {method_count}")
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        method_count = collention.count_documents({'method': method})
+        print(f"\tmethod {method}: {method_count}")
 
-check_get = collention.count_documents({'method': 'GET', 'path': "/status"})
-print(f"{check_get} status check")
+    check_get = collention.count_documents({'method': 'GET', 'path': "/status"})
+    print(f"{check_get} status check")
