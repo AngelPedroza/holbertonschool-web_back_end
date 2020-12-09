2f100f798f113b2bc42ae9f37e82d2649222f87d
Angel Pedroza <angel.pedroza@rappi.com>
2020-12-08 21:11:47 -0500

    [add] task 8

--- 0x0D-NoSQL/8-all.py
+++ 0x0D-NoSQL/8-all.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
+""" MongoDB find python usage"""
 
 
 def list_all(mongo_collection):
     """Return all documents"""
-    return mongo_collection.find()
+    return [] if mongo_collection.count() == 0 else mongo_collection.find()
