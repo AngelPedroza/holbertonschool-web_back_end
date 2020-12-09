5d3b740c2fcb6befea62217c647f778f90ec32ac
Angel Pedroza <angel.pedroza@rappi.com>
2020-12-08 22:50:53 -0500

    [add] task 15

---
+++ 0x0D-NoSQL/102-log_stats.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Print info in a collection"""
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    """ Make a check for all elements in a collention """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collention = client.logs.nginx
+
+    print(f"{collention.estimated_document_count()} logs")
+
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        method_count = collention.count_documents({'method': method})
+        print(f"\tmethod {method}: {method_count}")
+
+    check_get = collention.count_documents({'method': 'GET', 'path': "/status"})
+    print(f"{check_get} status check")
+
+    print("IPs:")
+    top_ips = collention.aggregate([
+        {"$group":
+            {
+                "_id": "$ip",
+                "count": {"$sum": 1}
+            }
+        },
+        {"$sort": {"count": -1}},
+        {"$limit": 10},
+        {"$project": {
+            "_id": 0,
+            "ip": "$_id",
+            "count": 1
+        }}
+    ])
+    for ip in top_ips:
+        print(f"\t{ip.get('ip')}: {ip.get('count')}")
