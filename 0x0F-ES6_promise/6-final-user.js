1f4a45565c42a639048abae9e52932c1592f514e
Angel Pedroza <angel.pedroza@rappi.com>
2020-12-16 14:49:48 -0500

    [Add] Task 6

---
+++ 0x0F-ES6_promise/6-final-user.js
@@ -0,0 +1,34 @@
+import signUpUser from "./4-user-promise"
+import uploadPhoto from "./5-photo-reject"
+
+async function handleProfileSignup(firstName, lastName, fileName) {
+  const json1 = {
+    status: 'pending'
+  };
+
+  const json2 = {
+    status: 'pending'
+  };
+
+  try {
+    const promise_sing = await signUpUser(firstName, lastName);
+    json1.status = 'fulfilled';
+    json1.value = promise_sing;
+  } catch (err) {
+    json1.status = 'rejected'
+    json1.value = err.toString();
+  }
+
+  try {
+    const up_promise = await uploadPhoto(fileName);
+    json2.status = 'fulfilled';
+    json2.value = promise_sing;
+  } catch (err) {
+    json2.status = 'rejected'
+    json2.value = err.toString();
+  }
+
+  return [json1, json2]
+}
+
+export default handleProfileSignup;
