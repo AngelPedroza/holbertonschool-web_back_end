2615c26f12f5c7041871610b0169439382604147
Angel Pedroza <angel.pedroza@rappi.com>
2020-12-16 14:56:24 -0500

    [Fix] Task 6 Styles x2

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -1,30 +1,29 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 async function handleProfileSignup(firstName, lastName, fileName) {
   const json1 = {
-    status: 'pending'
+    status: 'pending ',
   };
-
   const json2 = {
-    status: 'pending'
+    status: 'pending ',
   };
 
   try {
-    const promise_sing = await signUpUser(firstName, lastName);
+    const signup_promise = await signUpUser(firstName, lastName);
     json1.status = 'fulfilled';
-    json1.value = promise_sing;
+    json1.value = signup_promise;
   } catch (err) {
-    json1.status = 'rejected'
+    json1.status = 'rejected';
     json1.value = err.toString();
   }
 
   try {
-    const up_promise = await uploadPhoto(fileName);
+    const upload_promise = await uploadPhoto(fileName);
     json2.status = 'fulfilled';
-    json2.value = promise_sing;
+    json2.value = upload_promise;
   } catch (err) {
-    json2.status = 'rejected'
+    json2.status = 'rejected';
     json2.value = err.toString();
   }
 
