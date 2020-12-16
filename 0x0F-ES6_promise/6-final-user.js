9694b960de290f5ca4546fdeeea7fb1515fb65cd
Angel Pedroza <angel.pedroza@rappi.com>
2020-12-16 14:59:46 -0500

    [Fix] Esta mierda que no pasan los estilos

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -2,32 +2,32 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 async function handleProfileSignup(firstName, lastName, fileName) {
-  const json1 = {
+  const promise1 = {
     status: 'pending ',
   };
-  const json2 = {
+  const promise2 = {
     status: 'pending ',
   };
 
   try {
-    const sigpromise = await signUpUser(firstName, lastName);
-    json1.status = 'fulfilled';
-    json1.value = sigpromise;
+    const signup = await signUpUser(firstName, lastName);
+    promise1.status = 'fulfilled';
+    promise1.value = signup;
   } catch (err) {
-    json1.status = 'rejected';
-    json1.value = err.toString();
+    promise1.status = 'rejected';
+    promise1.value = err.toString();
   }
 
   try {
-    const upload_promise = await uploadPhoto(fileName);
-    json2.status = 'fulfilled';
-    json2.value = upload_promise;
+    const upload = await uploadPhoto(fileName);
+    promise2.status = 'fulfilled';
+    promise2.value = upload;
   } catch (err) {
-    json2.status = 'rejected';
-    json2.value = err.toString();
+    promise2.status = 'rejected';
+    promise2.value = err.toString();
   }
 
-  return [json1, json2];
+  return [promise1, promise2];
 }
 
 export default handleProfileSignup;
