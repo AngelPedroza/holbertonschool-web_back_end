8e657a732977560eb198b175e9576b26a3910c5f
Angel Pedroza <angel.pedroza@rappi.com>
2021-01-06 16:58:17 -0500

    [Add] Task 2

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -4,10 +4,16 @@ function countStudents(path) {
   let content;
   try {
     content = fs.readFileSync(`${path}`, { encoding: 'utf8' });
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
     const chainStudents = content.split('\n');
-    const students = chainStudents.slice(1);
-    console.log(`Number of students: ${students.length}`);
+    let students = chainStudents.filter((item) => item);
 
+    const NUMBER_OF_STUDENTS = students.length ? students.length - 1 : 0;
+    console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
+
+    students = students.slice(1)
     const dict = {};
     students.forEach((element) => {
       const list = element.split(',');
@@ -22,8 +28,6 @@ function countStudents(path) {
         console.log(`Number of students in ${i}: ${dict[i].length}. List: ${dict[i].join(', ')}`);
       }
     }
-  } catch (err) {
-    throw new Error('Cannot load the database');
-  }
+  
 }
 module.exports = countStudents;
