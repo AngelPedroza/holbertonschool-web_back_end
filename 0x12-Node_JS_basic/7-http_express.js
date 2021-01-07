9183a9286240ec421daab0d18a16bd0e72575285
Angel Pedroza <angel.pedroza@rappi.com>
2021-01-06 21:02:10 -0500

    [Add] Task 7

---
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async')
+const args = process.argv.slice(2);
+
+const app = express();
+const PORT = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  const msg = 'This is the list of our students\n';
+  try {
+    const students = await countStudents(args[0]);
+    res.send(`${msg}${students.join('\n')}`);
+  } catch (error) {
+    res.send(`${msg}${error.message}`);
+  }
+});
+
+app.listen(PORT, () => {
+  //   console.log(`Example app listening at http://localhost:${port}`);
+});
+
+module.exports = app;
