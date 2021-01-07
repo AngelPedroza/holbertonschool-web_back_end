daee641f50298d1483827b8212cf97dee2870172
Angel Pedroza <angel.pedroza@rappi.com>
2021-01-06 20:32:42 -0500

    [Add] Task 6

---
+++ 0x12-Node_JS_basic/6-http_express.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const PORT = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(PORT, () => {
+    //   console.log(`Example app listening at http://localhost:${port}`);
+});
